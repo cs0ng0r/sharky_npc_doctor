062857ed7d26123e1a43977377103e99d5e072af
cs0ng0r <bikadicsongor@gmail.com>
2024-07-09 06:00:38 +0200

    Started new method

--- server/main.lua
+++ server/main.lua
@@ -1,14 +1,24 @@
 local ESX = exports.es_extended:getSharedObject()
 
-ESX.RegisterServerCallback('sharky_mentonpc:getOnlineAmbulance', function(source, cb)
-    local xPlayers = ESX.GetPlayers()
-    local onlineAmbulance = 0
-    for i = 1, #xPlayers do
-        local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-        if xPlayer.job.name == 'ambulance' then
-            onlineAmbulance = onlineAmbulance + 1
+local onlineAmbulance = 0
+
+Citizen.CreateThread(function()
+    while true do
+        local xPlayers = ESX.GetPlayers()
+        local _onlineAmbulance = 0
+        for i = 1, #xPlayers do
+            local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+            if xPlayer.job.name == 'ambulance' then
+                onlineAmbulance = onlineAmbulance + 1
+            end
         end
+        onlineAmbulance = _onlineAmbulance
+        TriggerServerEvent('sharky_mentonpc:setOnlineAmbulance', -1, onlineAmbulance)
+        Citizen.Wait(Config.Options.CheckInterval * 1000)
     end
+end)
+
+ESX.RegisterServerCallback('sharky_mentonpc:getOnlineAmbulance', function(source, cb)
     cb(onlineAmbulance)
 end)
 
